a7a8c11e5f0c970037f9afcc35e85643c6e70ae8
Rafa Egea <desarrollo.rafaegea@gmail.com>
2025-10-23 14:55:52 +0200

    Refactor ON-SIZE-ERROR program structure

--- ON-SIZE-ERROR.cbl
+++ ON-SIZE-ERROR.cbl
@@ -5,51 +5,140 @@
       * Tectonics: cobc
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       PROGRAM-ID. ON-SIZE-ERROR.
+      ******************************************************************
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-       77 NUMERO1                  PIC 9(3)      VALUE 200.
-       77 NUMERO2                  PIC 9(3)      VALUE 100.
-       77 NUMERO3                  PIC 9(1)      VALUE 3.
-       77 NUMERO4                  PIC 9(1)      VALUE 4.
-       77 RESULTADO                PIC 9(3)      VALUE ZEROES.
-       77 RESULTADO-GRANDE         PIC 9(5)      VALUE ZEROES.
-
+      *
+      ******************************************************************
+      *                       VARIABLES                                *
+      ******************************************************************
+      *
+       77 WK-NUMERO1                  PIC 9(3)      VALUE 200.
+       77 WK-NUMERO2                  PIC 9(3)      VALUE 100.
+       77 WK-NUMERO3                  PIC 9(1)      VALUE 3.
+       77 WK-NUMERO4                  PIC 9(1)      VALUE 4.
+       77 WK-RESULTADO                PIC 9(3)      VALUE ZEROES.
+       77 WK-RESULTADO-GRANDE         PIC 9(5)      VALUE ZEROES.
+      *
+      ******************************************************************
        PROCEDURE DIVISION.
+      *
+           PERFORM 1000-INICIO
+           PERFORM 2000-PROCESO
+           PERFORM 3000-FIN
+           .
+      *
+      ******************************************************************
+      *                       1000-INICIO                              *
+      *----------------------------------------------------------------*
+      *                                                                *
+      *                                                                *
+      ******************************************************************
+       1000-INICIO.
 
-       COMPUTE RESULTADO = NUMERO1 * NUMERO2
-            ON SIZE ERROR DISPLAY "NUMERO DEMASIADO GRANDE"
-               DISPLAY RESULTADO
-       END-COMPUTE.
+      ******************************************************************
+      *                       2000-PROCESO                             *
+      *----------------------------------------------------------------*
+      *                                                                *
+      *                                                                *
+      ******************************************************************
+       2000-PROCESO.
+      *
+           PERFORM 2100-PARRAFO
+           PERFORM 2200-PARRAFO
+           PERFORM 2300-PARRAFO
+           PERFORM 2400-PARRAFO
+           PERFORM 3000-FIN
+           .
+      *
+      ******************************************************************
+      *                   2100-                                        *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************
+           2100-PARRAFO.
+      *
+           COMPUTE WK-RESULTADO = WK-NUMERO1 * WK-NUMERO2
+                ON SIZE ERROR
+                DISPLAY
+                "NUMERO DEMASIADO GRANDE"
+                DISPLAY WK-RESULTADO
+           END-COMPUTE
+           .
+      *
+      ******************************************************************
+      *                   2200-                                        *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************
+           2200-PARRAFO.
+      *
+           COMPUTE
+           WK-RESULTADO = WK-NUMERO1 * WK-NUMERO2
+               ON SIZE ERROR
+               COMPUTE
+               WK-RESULTADO-GRANDE = WK-NUMERO1 * WK-NUMERO2
+                   DISPLAY
+                   WK-RESULTADO-GRANDE
+           END-COMPUTE
+           .
+      *
+      ******************************************************************
+      *                   2300-                                        *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************
+           2300-PARRAFO.
+      *
+           COMPUTE
+           WK-RESULTADO = WK-NUMERO1 * WK-NUMERO2
+               ON SIZE ERROR
+                   DISPLAY
+                   "EL NUMERO ES MUY GRANDE, NO SE "-
+                   "VISUALIZA ENTERO"
+                   DISPLAY
+                   "SE HA ESTABLECIDO EL VALOR POR DEFECTO (200)"
+                   MOVE 200                        TO WK-RESULTADO
+                       NOT ON SIZE ERROR
+                       DISPLAY WK-RESULTADO
+           END-COMPUTE
 
-       COMPUTE RESULTADO = NUMERO1 * NUMERO2
-           ON SIZE ERROR COMPUTE RESULTADO-GRANDE = NUMERO1 * NUMERO2
-               DISPLAY RESULTADO-GRANDE
-       END-COMPUTE.
+           DISPLAY WK-RESULTADO
+           .
+      *
+      ******************************************************************
+      *                   2400-                                        *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************
+           2400-PARRAFO.
+      *
+           COMPUTE
+           WK-RESULTADO = WK-NUMERO3 * WK-NUMERO4
+               ON SIZE ERROR
+                   DISPLAY
+                   "EL NUMERO ES MUY GRANDE, NO SE "-
+                   "VISUALIZA ENTERO"
+                   DISPLAY
+                   "SE HA ESTABLECIDO EL VALOR POR DEFECTO (200)"
+                   MOVE 200                    TO WK-RESULTADO
+                   NOT ON SIZE ERROR
+                   DISPLAY WK-RESULTADO
+           END-COMPUTE
+           .
+      *
+      ******************************************************************
+      *                   3000-FIN                                     *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************
+       3000-FIN.
+      *
+            STOP RUN
+            .
+      *
+      ******************************************************************
 
 
-       COMPUTE RESULTADO = NUMERO1 * NUMERO2
-           ON SIZE ERROR
-               DISPLAY "EL NUMERO ES MUY GRANDE, NO SE "-
-                       "VISUALIZA ENTERO"
-               DISPLAY "SE HA ESTABLECIDO EL VALOR POR DEFECTO (200)"
-               MOVE 200 TO RESULTADO
-               NOT ON SIZE ERROR
-               DISPLAY RESULTADO
-       END-COMPUTE.
-
-       DISPLAY RESULTADO.
-
-       COMPUTE RESULTADO = NUMERO3 * NUMERO4
-           ON SIZE ERROR
-               DISPLAY "EL NUMERO ES MUY GRANDE, NO SE "-
-                       "VISUALIZA ENTERO"
-               DISPLAY "SE HA ESTABLECIDO EL VALOR POR DEFECTO (200)"
-               MOVE 200 TO RESULTADO
-               NOT ON SIZE ERROR
-               DISPLAY RESULTADO
-       END-COMPUTE.
-
-       STOP RUN.
-       END PROGRAM YOUR-PROGRAM-NAME.
