2e77b840551d898b331af0fa958e1ad4264ca8c6
Rafa Egea <desarrollo.rafaegea@gmail.com>
2025-10-23 13:19:53 +0200

    Refactor CLASES.cbl for improved structure and clarity
    Un pequeño ejemplo de cómo estructurar un programa para recibir y cotejar datos mediante rangos con CLASS.

--- CLASES.cbl
+++ CLASES.cbl
@@ -5,136 +5,304 @@
       * Tectonics: cobc
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       PROGRAM-ID. CLASES.
+      ******************************************************************
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SPECIAL-NAMES.
-           CLASS A-G IS "A" THRU "G", "a" THRU "g", "7" THRU "9".
-           CLASS A-G-UPPER IS "A" THRU "G".
-           CLASS A-G-LOWER IS "a" THRU "g".
-           CLASS BINARIO IS ZERO THRU "1".
-           CLASS HEX IS ZERO THRU "9", "A" THRU "F", "a" THRU "f".
+      ******************************************************************
+      *                      CLASES                                    *
+      ******************************************************************
+           CLASS 
+               A-G 
+               IS "A" THRU "G", "a" THRU "g", "7" THRU "9".
+           CLASS 
+               A-G-UPPER 
+               IS "A" THRU "G".
+           CLASS 
+               A-G-LOWER 
+               IS "a" THRU "g".
+           CLASS 
+               BINARIO 
+               IS ZERO THRU "1".
+           CLASS 
+               HEX 
+               IS ZERO THRU "9", "A" THRU "F", "a" THRU "f".
+      ******************************************************************
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-           01 Valor1 PIC X(4) VALUE "ABCD".
-           01 Valor2 PIC X(4) VALUE "A9cD".
-           01 Valor3 PIC X(4) VALUE "0101".
-           01 Valor4 PIC X(6) VALUE "aC24FF".
+      ******************************************************************
+      *                      VARIABLES                                 *
+      ******************************************************************
+       01 WK-VALORES.
+           05 WK-VALOR1           PIC X(4)      VALUE "ABCD".
+           05 WK-VALOR2           PIC X(4)      VALUE "A9cD".
+           05 WK-VALOR3           PIC X(4)      VALUE "0101".
+           05 WK-VALOR4           PIC X(6)      VALUE "aC24FF".
+      ******************************************************************
        PROCEDURE DIVISION.
-       *> Solicita datos al usuario.
-       SolicitaValor1.
-       *> Solicita Valor 1.
-            DISPLAY "--------------------------------------------"
-            DISPLAY
-            "Introduce cuatro caracteres en mayúscula entre A y G:".
-            DISPLAY "--------------------------------------------"
-            ACCEPT Valor1.
-
-       CompruebaValor1.
-           IF Valor1 IS A-G-UPPER OR Valor1 IS A-G-LOWER THEN
-               DISPLAY "--------------------------------------------"
-               DISPLAY "Has escrito " Valor1 " y está en el rango"
-               DISPLAY "--------------------------------------------"
+      *
+            PERFORM 1000-INICIO
+            PERFORM 2000-PROCESO
+            PERFORM 3000-FIN
+            .
+      * 
+      ******************************************************************
+      *                       1000-INICIO                              *
+      *----------------------------------------------------------------*
+      *                                                                * 
+      *                                                                *
+      ****************************************************************** 
+       1000-INICIO.
+      *         
+               PERFORM 1100-INICIAR-VARIABLES.
+      *         
+      ******************************************************************
+      *                   1100-INICIAR-VARIABLES                       *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************    
+           1100-INICIAR-VARIABLES.
+      * 
+               MOVE SPACES                 TO  WK-VALOR1           
+               MOVE SPACES                 TO  WK-VALOR2              
+               MOVE SPACES                 TO  WK-VALOR3          
+               MOVE SPACES                 TO  WK-VALOR4              
+               .
+      *
+      ******************************************************************
+      *                       2000-PROCESO                             *
+      *----------------------------------------------------------------*
+      *                                                                *
+      *                                                                *
+      ******************************************************************
+       2000-PROCESO.
+      *      
+               PERFORM 2100-SOLICITA-VALOR1
+               PERFORM 2101-COMPRUEBA-VALOR1
+               PERFORM 2200-SOLICITA-VALOR2
+               PERFORM 2201-COMPRUEBA-VALOR2
+               PERFORM 2300-SOLICITA-VALOR3
+               PERFORM 2301-COMPRUEBA-VALOR3
+               PERFORM 2400-SOLICITA-VALOR4
+               PERFORM 2401-COMPRUEBA-VALOR4
+               PERFORM 3000-FIN
+               .
+      *
+      ******************************************************************
+      *                   2100-SOLICITA-VALOR1                         *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************
+           2100-SOLICITA-VALOR1.
+      *
+               DISPLAY 
+               "--------------------------------------------"
+               DISPLAY
+               "Introduce cuatro caracteres en mayúscula entre A y G:"
+               DISPLAY 
+               "--------------------------------------------"
+               ACCEPT WK-VALOR1
+               .
+      *
+      ******************************************************************
+      *                   2101-COMPRUEBA-VALOR1                        *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************
+           2101-COMPRUEBA-VALOR1. 
+      *
+               IF WK-VALOR1 
+                   IS A-G-UPPER OR WK-VALOR1 IS A-G-LOWER 
+                   THEN
+                   DISPLAY 
+                   "--------------------------------------------"
+                   DISPLAY 
+                   "Has escrito " WK-VALOR1 " y está en el rango"
+                   DISPLAY 
+                   "--------------------------------------------"
+               ELSE
+                   DISPLAY 
+                   "--------------------------------------------"
+                   DISPLAY 
+                   "Has escrito " WK-VALOR1 " y no está en el rango"
+                   DISPLAY 
+                   "--------------------------------------------"
+               END-IF
+               .
+      *
+      ******************************************************************
+      *                   2200-COMPRUEBA-VALOR1                        *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************
+           2200-SOLICITA-VALOR2.                        
+      *
+               DISPLAY
+               "Introduce cuatro caracteres:"
+               DISPLAY 
+               "--------------------------------------------"
+               ACCEPT WK-VALOR2
+               .
+      ******************************************************************
+      *                   2201-COMPRUEBA-VALOR2                        *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************
+       2201-COMPRUEBA-VALOR2.
+      *
+           IF WK-VALOR2 
+               IS A-G 
+               THEN
+               DISPLAY 
+               "--------------------------------------------"
+               DISPLAY 
+               "Has escrito " WK-VALOR2 " y está en el rango"
+               DISPLAY 
+               "--------------------------------------------"
            ELSE
-               DISPLAY "--------------------------------------------"
-               DISPLAY "Has escrito " Valor1 " y no está en el rango"
-               DISPLAY "--------------------------------------------"
-           END-IF.
-
-       SolicitaValor2.
-       *> Solicita Valor2.
-            DISPLAY
-            "Introduce cuatro caracteres:".
-            DISPLAY "--------------------------------------------"
-            ACCEPT Valor2.
-
-       CompruebaValor2.
-
-           IF Valor2 IS A-G THEN
-               DISPLAY "--------------------------------------------"
-               DISPLAY "Has escrito " Valor2 " y está en el rango"
-               DISPLAY "--------------------------------------------"
+               DISPLAY 
+               "--------------------------------------------"
+               DISPLAY 
+               "Has escrito " WK-VALOR2 " y no está en el rango"
+               DISPLAY 
+               "--------------------------------------------"
+               DISPLAY 
+               "INTÉNTALO DE NUEVO"
+               DISPLAY 
+               "--------------------------------------------"
+               ACCEPT WK-VALOR2
+               DISPLAY 
+               "--------------------------------------------"
+           IF WK-VALOR2 
+               IS A-G 
+               THEN
+               DISPLAY
+               "--------------------------------------------"
+               DISPLAY
+               "Has escrito " WK-VALOR2 " y está en el rango"
+               DISPLAY
+               "--------------------------------------------"
            ELSE
-               DISPLAY "--------------------------------------------"
-               DISPLAY "Has escrito " Valor2 " y no está en el rango"
-               DISPLAY "--------------------------------------------"
-               DISPLAY "INTÉNTALO DE NUEVO"
-               DISPLAY "--------------------------------------------"
-               ACCEPT Valor2
-                   IF Valor2 IS A-G THEN
-                       DISPLAY
+               DISPLAY
                "--------------------------------------------"
-                       DISPLAY
-                       "Has escrito " Valor2 " y está en el rango"
-                       DISPLAY
-               "--------------------------------------------"
-                   ELSE
-                       DISPLAY
-               "--------------------------------------------"
-                       DISPLAY
+               DISPLAY
                "Has escrito dos veces y fuera de rango"
-                       DISPLAY
+               DISPLAY
                "--------------------------------------------"
-                       DISPLAY "ÚLTIMO INTENTO"
-                       DISPLAY
+               DISPLAY "ÚLTIMO INTENTO"
+               DISPLAY
                "--------------------------------------------"
-                       ACCEPT Valor2
-                       DISPLAY
+               ACCEPT WK-VALOR2
+               DISPLAY
                "--------------------------------------------"
-                           IF Valor2 IS A-G THEN
-                           DISPLAY
+           IF WK-VALOR2 
+               IS A-G 
+               THEN
+               DISPLAY
                "--------------------------------------------"
-                           DISPLAY
-                           "Has escrito " Valor2 " y está en el rango"
-                           DISPLAY
+               DISPLAY
+               "Has escrito " WK-VALOR2 " y está en el rango"
+               DISPLAY
                "--------------------------------------------"
-                           ELSE
-                           DISPLAY
+           ELSE
+               DISPLAY
                "--------------------------------------------"
-                           DISPLAY
+               DISPLAY
                "Has escrito tres veces y fuera de rango"
-                           DISPLAY
+               DISPLAY
                "--------------------------------------------"
-
-
-           END-IF.
-
-       SolicitaValor3.
-       *> Solicita Valor3.
-            DISPLAY "Introduce un binario de 4 dígitos:".
-            DISPLAY "--------------------------------------------"
-            ACCEPT Valor3.
-
-       CompruebaValor3.
-
-           IF Valor3 IS BINARIO THEN
-               DISPLAY "--------------------------------------------"
-               DISPLAY "Has escrito " Valor3 " y está en el rango"
-               DISPLAY "--------------------------------------------"
+           END-IF
+           .
+      *
+      ******************************************************************
+      *                   2300-SOLICITA-VALOR3                         *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************
+           2300-SOLICITA-VALOR3.                        
+      *
+               DISPLAY 
+               "Introduce un binario de 4 dígitos:"
+               DISPLAY 
+               "--------------------------------------------"
+               ACCEPT WK-VALOR3
+               .
+      *
+      ******************************************************************
+      *                   2301-COMPRUEBA-VALOR3                        *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************
+       2301-COMPRUEBA-VALOR3.
+      *
+           IF WK-VALOR3 
+               IS BINARIO 
+               THEN
+               DISPLAY 
+               "--------------------------------------------"
+               DISPLAY 
+               "Has escrito " WK-VALOR3 " y está en el rango"
+               DISPLAY 
+               "--------------------------------------------"
            ELSE
-               DISPLAY "--------------------------------------------"
-               DISPLAY "Has escrito " Valor3 " y no está en el rango"
-               DISPLAY "--------------------------------------------"
-           END-IF.
-
-       SolicitaValor4.
-       *> Solicita Valor4.
-            DISPLAY "Introduce un hexadecimal de 6 dígitos:".
-            DISPLAY "--------------------------------------------"
-            ACCEPT Valor4.
-
-       CompruebaValor4.
-
-           IF Valor4 IS HEX THEN
-               DISPLAY "--------------------------------------------"
-               DISPLAY "Has escrito " Valor4 " y está en el rango"
-               DISPLAY "--------------------------------------------"
-           ELSE
-               DISPLAY "--------------------------------------------"
-               DISPLAY "Has escrito " Valor4 " y no está en el rango"
-               DISPLAY "--------------------------------------------"
-           END-IF.
-
+               DISPLAY 
+               "--------------------------------------------"
+               DISPLAY 
+               "Has escrito " WK-VALOR3 " y no está en el rango"
+               DISPLAY 
+               "--------------------------------------------"
+           END-IF
+               .
+      *         
+      ******************************************************************
+      *                   2400-SOLICITA-VALOR4                         *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************
+           2400-SOLICITA-VALOR4.                        
+      *
+               DISPLAY 
+               "Introduce un hexadecimal de 6 dígitos:"
+               DISPLAY 
+               "--------------------------------------------"
+               ACCEPT WK-VALOR4
+               .
+      *
+      ******************************************************************
+      *                   2401-COMPRUEBA-VALOR4                        *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************
+           2401-COMPRUEBA-VALOR4.                        
+      *
+               IF WK-VALOR4 
+                   IS HEX 
+                   THEN
+                   DISPLAY 
+                   "--------------------------------------------"
+                   DISPLAY 
+                   "Has escrito " WK-VALOR4 " y está en el rango"
+                   DISPLAY 
+                   "--------------------------------------------"
+               ELSE
+                   DISPLAY 
+                   "--------------------------------------------"
+                   DISPLAY 
+                   "Has escrito " WK-VALOR4 " y no está en el rango"
+                   DISPLAY 
+                   "--------------------------------------------"
+               END-IF
+               .
+      *
+      ******************************************************************
+      *                       3000-FIN                                 *
+      *----------------------------------------------------------------*
+      *                                                                *
+      ******************************************************************
+       3000-FIN.
+      *
             STOP RUN.
-       END PROGRAM YOUR-PROGRAM-NAME.
+      *
+      ******************************************************************
